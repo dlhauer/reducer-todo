94bb3952ee6a6915b204a473265ff274bb7afb37
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-10 12:47:03 -0800

    Components, imports and exports

---
+++ reducer-todo-dan/src/components/TodoForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TodoForm(props) {
+  return (
+    <h2>TodoForm</h2>
+  )
+}
+
+export default TodoForm;
---
+++ reducer-todo-dan/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from './Todo';
+
+function TodoList(props) {
+  return (
+    <div>
+      <h2>TodoList</h2>
+      <Todo/>
+    </div>
+  )
+}
+
+export default TodoList;
