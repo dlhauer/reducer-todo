ef6bc93e802c47becfb2ddc52233e8bea43a4ec5
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-10 12:40:00 -0800

    todoReducer

---
+++ reducer-todo-dan/src/reducers/todoReducer.js
@@ -0,0 +1,26 @@
+export const initialState = [
+    {
+      item: 'Eat breakfast',
+      completed: false,
+      id: 5053972
+    },
+    {
+      item: 'Eat lunch',
+      completed: false,
+      id: 5053972
+    },
+    {
+      item: 'Eat dinner',
+      completed: false,
+      id: 5053972
+    },
+];
+
+export const todoReducer = (state, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+
+}
+
